49e50ae6c25880bc7f1672608b6eabc94f14eff2
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-05-15 12:39:07 -0700

    Start WIP on exercise 34

---
+++ 34-dynamic-array/README.md
@@ -0,0 +1,16 @@
+# Exercise 34: Dynamic Array
+
+## Notes
+
+- TODO
+
+## Extra Credit
+
+- [ ] 1. Improve the unit tests to cover more of the operations, and test them using a for-loop to ensure that they work.
+  - TODO
+- [ ] 2. Research what it would take to implement bubble sort and merge sort for DArray, but don't do it yet. I'll be implementing DArray algorithms next, so you'll do this then.
+  - TODO
+- [ ] 3. Write some performance tests for common operations and compare them to the same operations in `List`. You did some of this already, but this time, write a unit test that repeatedly does the operation in question, and then in the main runner, do the timing.
+  - TODO
+- [ ] 4. Look at how the `DArray_expand` is implemented using a constant increase (size + 300). Typically, dynamic arrays are implemented with a multiplicative increase (size \* 2), but I've found this to cost needless memory for no real performance gain. Test my assertion and see when you'd want a multiplicative increase instead of a constant increase.
+  - TODO
---
+++ 34-dynamic-array/modded/src/lcthw/darray.h
@@ -0,0 +1,96 @@
+/*
+This header file is showing you a new technique where I put
+`static inline` functions right in the header. These function
+definitions will work similar to the `#define` macros that you've
+been making, but they're cleaner and easier to write. If you need
+to create a block of code for a macro and you don't need code
+generation, then use a `static inline` function.
+
+Compare this technique to the `LIST_FOREACH` that generates a proper
+`for-loop` for a list. This would be impossible to do with a
+`static inline` function because it actually has to generate the
+inner block of code for the loop. The only way to do that is with a
+callback function, but that's not as fast and it's harder to use.
+*/
+
+#ifndef _DArray_h
+#define _DArray_h
+#include <stdlib.h>
+#include <assert.h>
+#include <lcthw/dbg.h>
+
+typedef struct DArray {
+    int end;
+    int max;
+    size_t element_size;
+    size_t expand_rate;
+    void **contents;
+} DArray;
+
+DArray *DArray_create(size_t element_size, size_t initial_max);
+
+void DArray_destroy(DArray *array);
+
+void DArray_clear(DArray *array);
+
+int DArray_expand(DArray *array);
+
+int DArray_contract(DArray *array);
+
+int DArray_push(DArray *array, void *el);
+
+void *DArray_pop(DArray *array);
+
+void DArray_clear_destroy(DArray *array);
+
+#define DArray_last(A) ((A)->contents[(A)->end - 1])
+#define DArray_first(A) ((A)->contents[0])
+#define DArray_end(A) ((A)->end)
+#define DArray_count(A) DArray_end(A)
+#define DArray_max(A) ((A)->max)
+
+#define DEFAULT_EXPAND_RATE 300
+
+static inline void DArray_set(DArray *array, int i, void *el)
+{
+    check(i < array->max, "darray attempt to set past max");
+    if (i > array->end)
+        array->end = i;
+    array->contents[i] = el;
+error:
+    return;
+}
+
+static inline void *DArray_get(DArray *array, int i)
+{
+    check(i < array->max, "darray attempt to get past max");
+    return array->contents[i];
+error:
+    return NULL;
+}
+
+static inline void *DArray_remove(DArray *array, int i)
+{
+    void *el = array->contents[i];
+
+    array->contents[i] = NULL;
+
+    return el;
+}
+
+static inline void *DArray_new(DArray *array)
+{
+    check(
+        array->element_size > 0,
+        "Can't use DArray_new on 0 size darrays."
+    );
+
+    return calloc(1, array->element_size);
+
+error:
+    return NULL;
+}
+
+#define DArray_free(E) free((E))
+
+#endif
